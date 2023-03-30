526dd801146205f69caaa95260caf71b8a0f39a1
CassyUMR <mabrismacr@outlook.com>
2023-03-30 09:49:52 -0600

    Commit #1

---
+++ js/script.js
@@ -0,0 +1,153 @@
+function Frase() {
+    var num = Math.floor((Math.random() * 50) + 1);
+    if (num == 1) {
+        document.getElementById('cont1').innerHTML='La gente tarda poco en juzgar, pero mucho en corregirse.'
+    }
+    if (num == 2) {
+        document.getElementById('cont1').innerHTML='No dependas de nadie en este mundo... \n Porque hasta tu propia sombra te abandona en la oscuridad'
+    }
+    if (num == 3) {
+        document.getElementById('cont1').innerHTML='Es increíble lo que una persona puede llegar a olvidar...\n Pero es más sorprendente lo que puede llegar a esconder'
+    }
+    if (num == 4) {
+        document.getElementById('cont1').innerHTML='Únicamente dejando entrar a extraños, podemos encontrar nuevas formas de ser nosotros mismos.'
+    }
+    if (num == 5) {
+        document.getElementById('cont1').innerHTML='El corazón puede ser débil, pero algunas veces es todo lo que necesitamos'
+    }
+    if (num == 6) {
+        document.getElementById('cont1').innerHTML='Si de verdad existe el mal en este mundo, éste reside en el mismo corazón del hombre'
+    }
+    if (num == 7) {
+        document.getElementById('cont1').innerHTML='Nadie es innecesario.'
+    }
+    if (num == 8) {
+        document.getElementById('cont1').innerHTML='No puedes deshacer lo que ya has hecho, pero sí puedes enfrentarlo'
+    }
+    if (num == 9) {
+        document.getElementById('cont1').innerHTML='Cuando la vida se pone difícil, te levantas, peleas, y le enseñas quien está al cargo!'
+    }
+    if (num == 10) {
+        document.getElementById('cont1').innerHTML='No importa cuán oscura que sea la noche, el día siempre vuelve a aparecer'
+    }
+    if (num == 11) {
+        document.getElementById('cont1').innerHTML='Al final, solo lamentamos lo que no hemos hecho'
+    }
+    if (num == 12) {
+        document.getElementById('cont1').innerHTML='Vi la muerte en mis sueños muchas veces, pero no he muerto. Fui mejor que mis sueños, mejor que mis pesadillas. Pero para descubrirlo... \n tuve que enfrentarlos a todos y tuve que pasar por lo peor para probarme a mí mismo que fui el mejor. Esta es mi realidad'
+    }
+    if (num == 13) {
+        document.getElementById('cont1').innerHTML='Los hombres buenos tienen intenciones buenas. Solo que no siempre terminamos haciendo el bien'
+    }
+    if (num == 14) {
+        document.getElementById('cont1').innerHTML='Que alguien se haya ido no significa que te haya dejado'
+    }
+    if (num == 15) {
+        document.getElementById('cont1').innerHTML='La grandeza está en lo que hacemos, no en quienes somos'
+    }
+    if (num == 16) {
+        document.getElementById('cont1').innerHTML='Hay una línea muy difusa entre valentía y estupidez'
+    }
+    if (num == 17) {
+        document.getElementById('cont1').innerHTML='La esperanza es lo que nos hace fuertes. Es la razón del por qué estamos aquí. Es por lo que peleamos cuando todo lo demás está perdido'
+    }
+    if (num == 18) {
+        document.getElementById('cont1').innerHTML='La vida se trata de determinación. El resultado es secundario'
+    }
+    if (num == 19) {
+        document.getElementById('cont1').innerHTML='Incluso en tiempos oscuros, no podemos renunciar a las cosas que nos hacen humanos'
+    }
+    if (num == 20) {
+        document.getElementById('cont1').innerHTML='El final no es más importante que cualquiera de los momentos que nos llevaron hasta él'
+    }
+    if (num == 21) {
+        document.getElementById('cont1').innerHTML='El conocimiento es inútil si no se utiliza'
+    }
+    if (num == 22) {
+        document.getElementById('cont1').innerHTML='No importa que tan malo se un hombre, siempre saben diferenciar entre el bien y el mal'
+    }
+    if (num == 23) {
+        document.getElementById('cont1').innerHTML='No desees que fuera más fácil, mejora para que sea menos dificil'
+    }
+    if (num == 24) {
+        document.getElementById('cont1').innerHTML='Toda mentira contiene la verdad, y toda verdad contiene una mentira'
+    }
+    if (num == 25) {
+        document.getElementById('cont1').innerHTML='Fue el destino quien me trajo aquí, pero soy yo quien trazo mi propio camino'
+    }
+    if (num == 26) {
+        document.getElementById('cont1').innerHTML='La mitad de lo que conocemos es mentira… La otra mitad, una mentira bien construida'
+    }
+    if (num == 27) {
+        document.getElementById('cont1').innerHTML='Cuanto más te acerques a la luz, mayor será tu sombra'
+    }
+    if (num == 28) {
+        document.getElementById('cont1').innerHTML='Los sacrificios los decides tú. Las pérdidas las deciden por ti'
+    }
+    if (num == 29) {
+        document.getElementById('cont1').innerHTML='¿Hasta dónde estás dispuesto a llegar para salvar a los que más quieres?'
+    }
+    if (num == 30) {
+        document.getElementById('cont1').innerHTML='No sé si creo en el paraíso, pero creo en los ángeles'
+    }
+    if (num == 31) {
+        document.getElementById('cont1').innerHTML='Todo juego tiene una historia… Pero sólo uno es una leyenda'
+    }
+    if (num == 32) {
+        document.getElementById('cont1').innerHTML='Hay gente que resulta útil y hay gente que no; Y la gente que no es útil debe morir'
+    }
+    if (num == 33) {
+        document.getElementById('cont1').innerHTML='¡Mantén tu determinación!'
+    }
+    if (num == 34) {
+        document.getElementById('cont1').innerHTML='Odio las tumbas'
+    }
+    if (num == 35) {
+        document.getElementById('cont1').innerHTML='No te lamentes, sé mejor'
+    }
+    if (num == 36) {
+        document.getElementById('cont1').innerHTML='No te hagas ilusiones y nunca te decepsionarás'
+    }
+    if (num == 37) {
+        document.getElementById('cont1').innerHTML='Detrás de los mitos siempre hay algo de verdad'
+    }
+    if (num == 38) {
+        document.getElementById('cont1').innerHTML='Querer algo no te da el derecho de tenerlo'
+    }
+    if (num == 39) {
+        document.getElementById('cont1').innerHTML='Si te encuentras en un pozo, lo primero que tienes que hacer es dejar de excavar'
+    }
+    if (num == 40){
+        document.getElementById('cont1').innerHTML='No necesito ningún arma; ¡mis amigos son mi poder!'
+    }
+    if (num == 41){
+        document.getElementById('cont1').innerHTML='No necesito ningún arma; ¡mis amigos son mi poder!'
+    }
+    if (num == 42){
+        document.getElementById('cont1').innerHTML='Quiero vengaza, no resultados'
+    }
+    if (num == 43){
+        document.getElementById('cont1').innerHTML='La venganza no te satisfaserá'
+    }
+    if (num == 44){
+        document.getElementById('cont1').innerHTML='Si todo es un sueño, no me despiertes'
+    }
+    if (num == 45){
+        document.getElementById('cont1').innerHTML='La muerte por ironía resulta dolorosa'
+    }
+    if (num == 46){
+        document.getElementById('cont1').innerHTML='Las batallas más duras se pelean en la mente, no con la espada'
+    }
+    if (num == 47){
+        document.getElementById('cont1').innerHTML='El éxito no es el final, el error no es fatal: es el coraje para continuar lo que queda'
+    }
+    if (num == 48){
+        document.getElementById('cont1').innerHTML='No debo parar mientras haya fuerzas que busquen hacer daño'
+    }
+    if (num == 49){
+        document.getElementById('cont1').innerHTML='En realidad somos buenos ¿o solo lo creemos?'
+    }
+    if (num == 50){
+        document.getElementById('cont1').innerHTML='Eres mejor de lo que pareces. Intenta más'
+    }
+}
